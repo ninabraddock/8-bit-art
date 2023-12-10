3c0aa13f9cd9fd8b941389264cb5fab408ea0a50
ninabraddock <nina.braddock@uvm.edu>
2023-12-09 22:18:50 -0500

    copy over contents

---
+++ 8-bit/README.md
@@ -0,0 +1,12 @@
+# 8-Bit
+
+The given program displays a series of red, green, blue, yellow, magenta, and cyan designs. It reads data in from a file, making it easier to modify what is drawn.
+
+Your job is to play around with this way of drawing:
+* Look at `scene.txt` in `src/art` and compare it with the graphics display.
+* Read through the `Engine::readFromFile` to see how it works.
+    * Also notice the const `SIDE_LENGTH` field in `engine.h`. Change it to different values to see the effect.
+* Modify `scene.txt` to create a custom drawing.
+* Modify the code to interact with the scene in different ways.
+
+You have 30 minutes to work on this activity. After that time is up, we will have everyone share what they have: everyone will run their programs and walk around to see what other people made.
---
+++ 8-bit/src/framework/engine.cpp
@@ -0,0 +1,169 @@
+#include "engine.h"
+#include <fstream>
+
+using namespace std;
+
+Engine::Engine() : keys() {
+    this->initWindow();
+    this->initShaders();
+    this->initShapes();
+}
+
+Engine::~Engine() {}
+
+unsigned int Engine::initWindow(bool debug) {
+    // glfw: initialize and configure
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE);
+#endif
+    glfwWindowHint(GLFW_RESIZABLE, false);
+
+    window = glfwCreateWindow(width, height, "engine", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    // OpenGL configuration
+    glViewport(0, 0, width, height);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glfwSwapInterval(1);
+
+    return 0;
+}
+
+void Engine::initShaders() {
+    // load shader manager
+    shaderManager = make_unique<ShaderManager>();
+
+    // Load shader into shader manager and retrieve it
+    shapeShader = this->shaderManager->loadShader("../res/shaders/shape.vert", "../res/shaders/shape.frag",  nullptr, "shape");
+
+    // Configure text shader and renderer
+    textShader = shaderManager->loadShader("../res/shaders/text.vert", "../res/shaders/text.frag", nullptr, "text");
+    fontRenderer = make_unique<FontRenderer>(shaderManager->getShader("text"), "../res/fonts/MxPlus_IBM_BIOS.ttf", 24);
+
+    // Set uniforms
+    textShader.setVector2f("vertex", vec4(100, 100, .5, .5));
+    shapeShader.use();
+    shapeShader.setMatrix4("projection", this->PROJECTION);
+}
+
+void Engine::initShapes() {
+    // Relative file path from inside cmake-build-debug folder to the txt file
+    readFromFile("../res/art/scene.txt");
+}
+
+void Engine::processInput() {
+    glfwPollEvents();
+
+    // Set keys to true if pressed, false if released
+    for (int key = 0; key < 1024; ++key) {
+        if (glfwGetKey(window, key) == GLFW_PRESS)
+            keys[key] = true;
+        else if (glfwGetKey(window, key) == GLFW_RELEASE)
+            keys[key] = false;
+    }
+
+    // Close window if escape key is pressed
+    if (keys[GLFW_KEY_ESCAPE])
+        glfwSetWindowShouldClose(window, true);
+
+    // Mouse position saved to check for collisions
+    glfwGetCursorPos(window, &MouseX, &MouseY);
+
+    // If mouse overlaps with spawn button, change color
+    // Mouse position is inverted because the origin of the window is in the top left corner
+    MouseY = height - MouseY; // Invert y-axis of mouse position
+    bool mousePressed = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
+
+    // Save mousePressed for next frame
+    mousePressedLastFrame = mousePressed;
+}
+
+void Engine::update() {
+    // Calculate delta time
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+}
+
+void Engine::render() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Set shader to use for all shapes
+    shapeShader.use();
+
+    for (unique_ptr<Shape> &square : squares) {
+        square->setUniforms();
+        square->draw();
+    }
+
+    glfwSwapBuffers(window);
+}
+
+void Engine::readFromFile(std::string filepath) {
+    ifstream ins(filepath);
+    if (!ins) {
+        cout << "Error opening file" << endl;
+    }
+    ins >> noskipws;
+    int xCoord = 0, yCoord = height - SIDE_LENGTH;
+    char letter;
+    bool draw;
+    color c;
+    while (ins >> letter) {
+        draw = true;
+        switch(letter) {
+            case 'r': c = color(1, 0, 0); break;
+            case 'g': c = color(0, 1, 0); break;
+            case 'b': c = color(0, 0, 1); break;
+            case 'y': c = color(1, 1, 0); break;
+            case 'm': c = color(1, 0, 1); break;
+            case 'c': c = color(0, 1, 1); break;
+            case ' ': c = color(0, 0, 0); break;
+            default: // newline
+                draw = false;
+                xCoord = 0;
+                yCoord -= SIDE_LENGTH;
+        }
+        if (draw) {
+            squares.push_back(make_unique<Rect>(shapeShader, vec2(xCoord + SIDE_LENGTH/2, yCoord + SIDE_LENGTH/2), vec2(SIDE_LENGTH, SIDE_LENGTH), c));
+            xCoord += SIDE_LENGTH;
+        }
+    }
+    ins.close();
+}
+
+bool Engine::shouldClose() {
+    return glfwWindowShouldClose(window);
+}
+
+GLenum Engine::glCheckError_(const char *file, int line) {
+    GLenum errorCode;
+    while ((errorCode = glGetError()) != GL_NO_ERROR) {
+        string error;
+        switch (errorCode) {
+            case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+            case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+            case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
+            case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
+            case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+            case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+        }
+        cout << error << " | " << file << " (" << line << ")" << endl;
+    }
+    return errorCode;
+}
